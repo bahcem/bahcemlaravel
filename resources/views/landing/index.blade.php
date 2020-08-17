24831e15e332c3ff79ee717f9d46869a58d36f28
seyfullahse <48314108+seyfullahse@users.noreply.github.com>
2020-08-17 13:32:03 +0300

    kvkk

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -5,8 +5,30 @@
     <div class="wrapper tabled">
         <div class="stage" id="page1">
             <div class="middled">
-                <h1><span class="thin">Landing</span><span class="thick">page</span></h1>
-                <span style="color: #a1aec7;">Lorem ipsum dolor sit amet.</span>
+                <h1><span class="thin">KVKK Politikamız</span></h1>
+                <span style="color: #a1aec7;">
+				KİŞİSEL VERİLERİN KORUNMASI VE İŞLENMESİ HAKKINDA AYDINLATMA METNİ
+Bahçema Gıda Pazarlama (“Bahçema”) olarak mevcut kanunlar ve evrensel değerler çerçevesinde kişisel verilerinizin güvenliğini mümkün olan en üst düzeyde önemsiyoruz. Bu çerçevede, Bahçema olarak çalışanlarımız, müşterilerimiz, potansiyel müşterilerimiz, iş ortaklarımız da dahil olmak üzere ilişkili tüm kişilere ait her türlü kişisel verinin işlenmesi hususunda 6698 sayılı Kişisel Verilerin Korunması Kanunu’na (“KVKK”) uygun olarak davrandığımız hususunda sizleri bilgilendirmek isteriz.
+
+1. Kişisel Veri Nedir?
+Kişisel Veri, KVKK kapsamında kimliği belirli veya belirlenebilir gerçek kişiye ilişkin her türlü bilgiyi ifade etmektedir. Başka bir deyişle, Kişisel Veri, sizi diğer kişilerden ayırt etmemize yarayan her türlü bilgi anlamına gelmektedir.
+2. Kişisel Verilerinizi Nasıl İşliyoruz?
+Kişisel verilerinizin işlenmesi, KVKK’da açıklandığı üzere tamamen veya kısmen otomatik olan ya da herhangi bir veri kayıt sisteminin parçası olmak kaydıyla otomatik olmayan yollarla elde edilmesi, kaydedilmesi, depolanması, muhafaza edimesi, değiştirilmesi, yeniden düzenlenmesi, açıklanması, aktarılması, devralınması, elde edilebilir hale getirilmesi, sınıflandırılması ya da kullanılmasının engellenmesi gibi veriler üzerinde gerçekleştirilen her türlü işlem anlamına gelmektedir.
+Kişisel verileriniz; Bahçema’nın sunmuş olduğu ürün ve hizmetlere, “www.bahcemapp.com” adreslerindeki internet siteleri veya Bahçem mobil uygulaması üzerinden ulaştığınızda; Şirketimizi ve/veya internet sitelerimizi ve/veya mobil uygulamalarımızı ziyaret ettiğinizde; çağrı merkezimizi aradığınızda işlenebilecektir.
+Veri işleme faaliyeti sözlü, yazılı ya da elektronik ortamlarda gerçekleştirilebilir. Hangi ortamda olursa olsun tüm veri işleme faaliyetleri yalnızca aşağıda belirtilen amaçlarla işlenmektedir.
+
+3. Kişisel Verilerinizi Hangi Amaçlarla İşliyoruz?
+Kişisel verileriniz, Bahçema tarafından size sunulan hizmet ve gerçekleştirilen faaliyetlere göre farklılık göstermekle birlikte KVKK’da belirtilen anlam ve kapsamda; Bahçema tarafından sizlere sunduğumuz ürün ve hizmetlerin sağlanabilmesi, geliştirilmesi ve/veya mükemmelleştirilmesi amacıyla Bahçema’nın ilgili birimleri ile paylaşılmakta; yine bu çerçevede kişiselleştirme, beklentiler, kullanım alışkanlıkları, beğeniler ve ihtiyaçlar çerçevesinde profil oluşturma; yeni hizmet kollarının geliştirilmesi; güvenliğin ve denetimin sağlanması, müşteri değerlendirme ve şikayet süreçlerinin yönetimi, tarafınıza çeşitli avantajların sağlanıp sunulabilmesi, satış, pazarlama ve benzer amaçlı her türlü iletişim mesajlarının gönderilmesi, uyum süreçleri, insan kaynakları süreçleri ve benzeri hizmetlerin hayata geçirilebilmesi amacıyla KVKK’nın 4., 5. ve 6. maddeleri çerçevesinde işlenmektedir.
+İşlenen kişisel verileriniz, yine yalnızca yukarıda belirtilen amaçlar çerçevesinde KVKK’nın 8. ve 9. maddelerinde belirtildiği şekilde yurt içine ve yurt dışına ve yetkili kamu kurum ve kuruluşları tarafından talep edilmesi halinde ve yine kanunlar çerçevesinde ilgili kurumlara aktarılabilecektir.
+
+5. Kişisel Veri Sahibi olarak haklarınız nelerdir?
+KVKK uyarınca sahip olduğunuz hakları kullanabilmek için ilgili talebinizi belirtip hello@bahcema.com adresinden tarafımıza ulaşarak, kişisel verilerinizin; a) işlenip işlenmediğini öğrenme, b) işlenmişse buna ilişkin bilgi talep etme, c) işlenme amacını ve bunların amacına uygun kullanılıp kullanılmadığını öğrenme, d) eksik veya yanlış işlenmişse düzeltilmesini isteme, e) KVKK’nın 7. Maddesinde öngörülen şartlar çerçevesinde silinmesini veya yok edilmesini isteme, f) aktarıldığı 3. kişilere yukarıda sayılan (d) ve (e) bentleri uyarınca yapılan işlemlerin bildirilmesini isteme, g) münhasıran otomatik sistemler ile analiz edilmesi nedeniyle aleyhinize bir sonucun ortaya çıkmasına itiraz etme, h) kanuna aykırı olarak işlenmesi sebebiyle zarara uğramanız halinde zararın giderilmesini talep etme hakkına sahipsiniz.
+Talebiniz ile ilgili olarak Bahçema sizden teyit amaçlı bilgiler talep edebilecektir. Bunun amacı kişisel veri sahibinin gerçekten kim olduğunu anlamak, bilgileri yalnızca doğru kişiyle paylaşmak adına tam olarak emin olmaktır.
+Öte yandan işlenen kişisel verilerinizin eksik veya yanlış işlenmesi halinde düzeltilmesini internet sitemizin “Hesabım” ve/veya mobil uygulamamızın “Profil” veya “Ayarlar” bölümünden hesabınıza erişerek gerekli değişiklik, güncelleme ve silme işlemlerini yapabilirsiniz. Hesabınızı bu yoldan silmeniz halinde Bahçema’nın KVKK’ya uygun olarak işlediği kişisel verilerinizi, ilgili mevzuat ve kanunlarda öngörülen süreler boyunca saklamasına ilişkin yükümlülüklerine ilişkin hakları saklıdır.
+Bu kapsamda öncelikle, işlemeye konu kişisel verilerin saklanması için ilgili mevzuatta belirli bir süre öngörülüp görülmediği tespit edilir. Yasal bir süre belirlenmişse, bu süreye uygun davranılır. Yasal süre öngörülmemişse, işleme amacının sona erdiği sebebiyle kişisel verileriniz, daha uzun süre saklanmasını gerektiren hukuki bir sebep yoksa silinir veya yok edilir.
+Son olarak, şirketimizin Kişisel Verilerin Korunması ile ilgili olarak uyguladığı güncel ilke politikalara daima web sitelerimizden erişim sağlayabilirsiniz.
+
+				</span>
             </div>
         </div>
     </div>
